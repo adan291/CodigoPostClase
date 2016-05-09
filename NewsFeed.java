eb0ab8e1ddd9ce01cbe2de9dad13dfcf5874e762
adan291 <adanx05@gmail.com>
2016-05-09 12:29:14 +0200

    0128-CommitInicial

---
+++ NewsFeed.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+/**
+import java.util.ArrayList;
+ * Write a description of class NewsFeed here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NewsFeed
+{
+    private ArrayList<MessagePost> messages;
+    private ArrayList<PhotoPost> photos; 
+
+    /**
+     * Constructor for objects of class NewsFeed
+     */
+    public NewsFeed()
+    {
+        messages = new ArrayList<>();
+        photos = new ArrayList<>();
+    }
+
+    public void addMessagePost(MessagePost message)
+    {
+
+    }
+
+    public void addPhotoPost(PhotoPost photo)
+    {
+
+    }
+
+    public void show()
+    {
+
+    }
+}
